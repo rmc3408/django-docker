819d54f57d2f60b6e82ba174f0f8c231fe38188d
rmc3408 <rmc3408@protonmail.com>
2023-10-09 18:35:59 -0400

    collectStatic fix

--- core/deployment.py
+++ core/deployment.py
@@ -19,7 +19,7 @@ MIDDLEWARE = [
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 params = { code.split('='):code.split('=')[1] for code in connection_string}
