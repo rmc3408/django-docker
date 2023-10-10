7c10dbd11d75d4bacfd17d530dc634694cb7fdbc
rmc3408 <rmc3408@protonmail.com>
2023-10-09 22:35:16 -0400

    on settings

--- core/settings.py
+++ core/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/' # look for static inside APPS folder
 
 STATICFILES_DIRS = [
