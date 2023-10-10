c85a0b71583b5870c29cb3af837538be45e08b49
rmc3408 <rmc3408@protonmail.com>
2023-10-10 08:42:59 -0400

    save to branch DEPLOY1-webapp-host

--- challenges/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.listMonths, name="named-list"),
-    path('<int:month>', views.monthly_byNum),
-    path('<str:month>', views.monthly, name="named-month"),
-]
