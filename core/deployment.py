c40cd59bc5acbc8efabe2880cf636c2df8113222
rmc3408 <rmc3408@protonmail.com>
2023-10-09 22:57:18 -0400

    manual values

--- core/deployment.py
+++ core/deployment.py
@@ -6,11 +6,12 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEBUG = False
 
 SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
-DEBUG = False
+ALLOWED_HOSTS = ['django-challenge-webapp.azurewebsites.net']
+CSRF_TRUSTED_ORIGINS = ['https://django-challenge-webapp.azurewebsites.net']
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -26,7 +27,7 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
--- core/wsgi.py
+++ core/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-setting_environment = 'core.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'core.settings'
+setting_environment = 'core.deployment' if 'PRODUCTION' in os.environ else 'core.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', setting_environment)
 
