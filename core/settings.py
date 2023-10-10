68c7c83c99ed705791c93395791c39eb9c8a0300
rmc3408 <rmc3408@protonmail.com>
2023-10-09 23:10:09 -0400

    insecure

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET'] | 'django-insecure-*zk$+hdi(dw*u*agx(av(%nvk+v1qjdaf2enr$@c5stqipj3tr'
+SECRET_KEY = 'django-insecure-*zk$+hdi(dw*u*agx(av(%nvk+v1qjdaf2enr$@c5stqipj3tr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
